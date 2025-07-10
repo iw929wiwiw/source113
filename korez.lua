740fbbdb53b2f9534dd75f245df8834ab4eed60f
iw929wiwiw <keinken81@gmail.com>
2025-07-10 20:12:09 +0300

    Add files via upload

---
+++ korez.lua
@@ -0,0 +1,18 @@
+-- BrainrotConfig + remote loader
+getgenv().BrainrotConfig = {
+  ["Garama And Madundung"] = true,
+  ["La Grande Combinasion"] = true,
+  ["Graipuss Medussi"] = true,
+  ["Los Tralaleritos"] = true,
+  ["Sammyni Spyderini"] = true,
+  ["La Vacca Saturno Saturnita"] = true,
+  ["Trenostruzzo Turbo 3000"] = false,
+  ["Matteo"] = false,
+  ["Odin Din Din Dun"] = false,
+  ["Tralalero Tralala"] = false,
+  ["Unclito Samito"] = false,
+  ["Girafa Celestre"] = false,
+  ["Cocofanto Elefanto"] = false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iw929wiwiw/Secret-Only/refs/heads/main/Secret%20Finder"))();
